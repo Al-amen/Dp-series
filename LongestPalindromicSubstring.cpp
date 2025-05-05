c4d46d79717bd61412d99cb1842f1c333a5b6420
Al-Amen <alamen15@cse.pstu.ac.bd>
2025-05-05 20:51:23 +0600

    https://leetcode.com/problems/longest-palindromic-substring/

---
+++ LongestPalindromicSubstring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    private:
+       bool f(string &s, int i, int j,vector<vector<bool>>&dp) {
+        
+         if(i >= j)return true;
+         if(dp[i][j] != 0)return dp[i][j];
+         if(s[i] == s[j]) {
+            return dp[i][j] = f(s,i+1,j-1,dp);
+         }
+         return dp[i][j] = 0;
+    
+       }
+    public:
+        string longestPalindrome(string s) {
+            
+            int n = s.size();
+            int sp = -1;
+            int mx = -1;
+            vector<vector<bool>>dp(n+1,vector<bool>(n+1,0));
+            for(int i = 0; i < n; i++) {
+                for(int j = i; j < n; j++) {
+                    if(f(s,i,j,dp)) {
+                        int len = j-i+1;
+    
+                        if(mx < len) {
+                            sp = i;
+                            mx = max(len,mx);
+                        }
+                    }
+                }
+            }
+    
+            return s.substr(sp,mx);
+        }
+    };
